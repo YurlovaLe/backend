2f1fc4eb2306ce867fde7aeddd5f011a00a721c1
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-11-20 21:33:26 +0700

    hw done

--- src/app.js
+++ src/app.js
@@ -1,56 +1,54 @@
-const http = require("http");
-const url = require("url");
-const getUsers = require("./modules/users");
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const userRouter = require('./routes/users');
+const bookRouter = require('./routes/books');
 
-const hostname = "127.0.0.1";
-const port = 3003;
+morgan.token('fullUrl', (request, response) => {
+  const protocol = request.protocol;
+  const host = request.hostname;
+  const url = request.originalUrl;
+  const port = process.env.PORT || PORT;
 
-const server = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-  const { hello, users } = parsedUrl.query;
-  console.log(parsedUrl.search)
+  const fullUrl = `${protocol}://${host}:${port}${url}`;
 
-  if (hello) {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.write(`Hello, ${hello}`);
-    res.end();
-
-    return;
-  }
-
-  if (hello === '') {
-    res.statusCode = 400;
-    res.setHeader("Content-Type", "text/plain");
-    res.write('Enter a name');
-    res.end();
-
-    return;
-  }
-
-  if (typeof(users) === 'string') {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.write(getUsers());
-    res.end();
-
-    return;
-  }
-
-  if (parsedUrl.search) {
-    res.statusCode = 500;
-    res.setHeader("Content-Type", "text/plain");
-    res.write('');
-    res.end();
-
-    return;
-  }
-
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
+  return fullUrl;
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Сервер запущен http://${hostname}:${port}/`);
+dotenv.config();
+
+const {
+  PORT = 3005,
+  API_URL = 'http://127.0.0.1',
+  MONGO_URL = 'mongodb://localhost:27017/backend'
+} = process.env;
+
+const connectMongoose = async () => {
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connectMongoose();
+
+const app = express();
+
+app.use(morgan(':method :fullUrl'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(userRouter);
+app.use(bookRouter);
+app.all('*', function (request, response) {
+  response.status(404);
+  response.send('Bad request');
+});
+
+app.listen(PORT, () => {
+    console.log(`Ссылка на сервер: ${API_URL}:${PORT}`);
 });
